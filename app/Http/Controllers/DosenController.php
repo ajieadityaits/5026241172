c859c90d56ebb2abc1906a73690e79bb48d95faa
ajieadityaits <ajieadityaits@gmail.com>
2026-04-20 09:51:01 +0700

    Pertemuan 7

---
+++ app/Http/Controllers/DosenController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+// import
+class DosenController extends Controller
+{
+    //
+public function index(){
+    return "<h1>Halo ini adalah method index</h1>, dalam controller DosenController. - www.malasngoding.com";
+}
+
+public function biodata(){
+    $nama = "Muhammad Ajie Aditya";
+    $pelajaran = ["Algoritma & Pemrograman","Kalkulus","Pemrograman Web"];
+    return view('biodata', ['nama' => $nama , 'matkul' => $pelajaran]);
+}
+
+}
